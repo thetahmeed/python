a09097b0c2ad9846c31eb8c114fd89ef60ea624d
Tahmeedul Islam <tahmeed.com.bd@gmail.com>
2024-06-22 00:06:23 +0600

    Create 04_numbers.py

---
+++ 04_numbers.py
@@ -0,0 +1,19 @@
+# int 
+x = 5
+
+# float
+y = 5.9
+
+# complex
+z = 1j
+
+# casting
+x = float(x)
+y = int(y)
+
+# we can use the type() function to check the data type of a variable
+print(type(x))
+
+# random number
+import random
+print(random.randrange(1, 10))
