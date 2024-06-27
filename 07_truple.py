1ff5b395f386cd4de08abf6a824d045b1551b241
Tahmeedul Islam <tahmeed.com.bd@gmail.com>
2024-06-27 23:45:55 +0600

    Create 07_truple.py

---
+++ 07_truple.py
@@ -0,0 +1,18 @@
+myTruple = ('Ironman', 'Thor', 'Hulk', 'Captain America', 'Black Widow', 'Hawkeye')
+print(myTruple)
+
+# When we say that tuples are ordered, it means that the items have a defined order, 
+# and that order will not change.
+
+# Tuples are unchangeable, meaning that we cannot change, add or remove items after the 
+# tuple has been created.
+
+# Allow duplicate values
+
+# one item tuple
+one_item_tuple = ('Ironman',)
+print(one_item_tuple)
+
+# Methods
+# count() -> Returns the number of times a specified value occurs in a tuple
+# index() -> Searches the tuple for a specified value and returns the position of where it was found
