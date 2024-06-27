f6545595ab3b9b6c50460eafed1c8120ea234df0
Tahmeedul Islam <tahmeed.com.bd@gmail.com>
2024-06-27 23:46:01 +0600

    Create 08_sets.py

---
+++ 08_sets.py
@@ -0,0 +1,42 @@
+mySet = {1, 2, 3, 4, 5}
+print(mySet)
+
+# Sets are unordered, meaning that the items have no defined order.
+# Sets are unchangeable, meaning that we cannot change the items after the set has been created.
+# Sets do not allow duplicate values.
+
+# Join sets
+set1 = {1, 2, 3}
+set2 = {4, 5, 6}
+set3 = set1.union(set2)
+print(set3)
+
+# The union() and update() methods joins all items from both sets.
+# The intersection() method keeps ONLY the duplicates.
+# The difference() method keeps the items from the first set that are not in the other set(s).
+# The symmetric_difference() method keeps all items EXCEPT the duplicates.
+# '|' is the union operator
+# '&' is the intersection operator
+# '-' is the difference operator
+# '^' is the symmetric difference operator
+
+# Methods
+# add()	 	Adds an element to the set
+# clear()	 	Removes all the elements from the set
+# copy()	 	Returns a copy of the set
+# difference()	-	Returns a set containing the difference between two or more sets
+# difference_update()	-=	Removes the items in this set that are also included in another, specified set
+# discard()	 	Remove the specified item
+# intersection()	&	Returns a set, that is the intersection of two other sets
+# intersection_update()	&=	Removes the items in this set that are not present in other, specified set(s)
+# isdisjoint()	 	Returns whether two sets have a intersection or not
+# issubset()	<=	Returns whether another set contains this set or not
+#  	<	Returns whether all items in this set is present in other, specified set(s)
+# issuperset()	>=	Returns whether this set contains another set or not
+#  	>	Returns whether all items in other, specified set(s) is present in this set
+# pop()	 	Removes an element from the set
+# remove()	 	Removes the specified element
+# symmetric_difference()	^	Returns a set with the symmetric differences of two sets
+# symmetric_difference_update()	^=	Inserts the symmetric differences from this set and another
+# union()	|	Return a set containing the union of sets
+# update()	|=	Update the set with the union of this set and others
